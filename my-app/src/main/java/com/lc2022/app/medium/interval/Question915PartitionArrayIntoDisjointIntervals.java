1e8b69aa13925d41d9875d417a0dfce8b73cecab
Rui Jiang <rui.jiang.dev@gmail.com>
2022-01-26 16:02:26 -0800

    Question 915 Partition Array into Disjoint Intervals

---
+++ my-app/src/main/java/com/lc2022/app/medium/interval/Question915PartitionArrayIntoDisjointIntervals.java
@@ -0,0 +1,33 @@
+package com.lc2022.app.medium.interval;
+
+public class Question915PartitionArrayIntoDisjointIntervals {
+  public int partitionDisjoint(int[] nums) {
+    if (nums == null) {
+      return -1;
+    }
+    if (nums.length <= 1) {
+      return 0;
+    }
+    int[] leftMax = new int[nums.length];
+    leftMax[0] = nums[0];
+    for(int i = 1; i < nums.length; i++) {
+      leftMax[i] = Math.max(leftMax[i-1], nums[i]);
+    }
+
+    int len = nums.length;
+    int[] rightMin = new int[nums.length];
+    rightMin[len-1] = nums[len -1];
+    for(int i = len-2; i >= 0; i--) {
+      rightMin[i] = Math.min(rightMin[i+1], nums[i]);
+    }
+
+    for(int i = 0; i < nums.length-1; i++) {
+      int left = leftMax[i];
+      int right = rightMin[i+1];
+      if(left <= right) {
+        return i+1;
+      }
+    }
+    return -1;
+  }
+}
---
+++ my-app/src/test/java/com/lc2022/app/medium/interval/Question915PartitionArrayIntoDisjointIntervalsTest.java
@@ -0,0 +1,36 @@
+package com.lc2022.app.medium.interval;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Question915PartitionArrayIntoDisjointIntervalsTest {
+  Question915PartitionArrayIntoDisjointIntervals solution = new Question915PartitionArrayIntoDisjointIntervals();
+
+  @Test
+  public void test1() {
+    int[] input = {5,0,3,8,6};
+    int output = solution.partitionDisjoint(input);
+    Assert.assertEquals(3, output);
+  }
+
+  @Test
+  public void test2() {
+    int[] input = {32,57,24,19,0,24,49,67,87,87};
+    int output = solution.partitionDisjoint(input);
+    Assert.assertEquals(7, output);
+  }
+
+  @Test
+  public void test3() {
+    int[] input = {24,11,49,80,63,8,61,22,73,85};
+    int output = solution.partitionDisjoint(input);
+    Assert.assertEquals(9, output);
+  }
+
+  @Test
+  public void test4() {
+    int[] input = {1,1,1,0,6,12};
+    int output = solution.partitionDisjoint(input);
+    Assert.assertEquals(4, output);
+  }
+}
